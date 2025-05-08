b4152343ddb0d2f42f06419214a0330d469d1f0d
Juan Di Minico <juanpablo.diminico.ext@idrica.com>
2025-05-08 10:37:38 -0300

    forma geometrica abs

---
+++ DevelopmentChalenge.Domain/FormaGeometrica/FormaGeometrica.cs
@@ -0,0 +1,18 @@
+﻿using DevelopmentChalenge.Domain.Enums;
+
+namespace DevelopmentChalenge.Domain.FormaGeometrica
+{
+    public abstract class FormaGeometrica
+    {
+        protected decimal _lado { get; }
+
+        protected FormaGeometrica(decimal lado)
+        {
+            _lado = lado;
+        }
+
+        public abstract TipoFormaGeometricaEnum Tipo { get; }
+        public abstract decimal CalcularArea();
+        public abstract decimal CalcularPerimetro();
+    }
+}
